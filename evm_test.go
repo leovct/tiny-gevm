bfb19000e14834f032c3178bf667e42aece6b95b
leovct <l009.vincent@gmail.com>
2024-07-17 11:50:50 +0200

    test: refactor tests

--- evm_test.go
+++ evm_test.go
@@ -14,89 +14,79 @@ func TestNewEVM(t *testing.T) {
 }
 
 func TestAdd(t *testing.T) {
-	// Create a new EVM.
-	evm := NewEVM()
-
-	// Push some elements to the stack.
-	// The stack should be equal to [0x1, 0x2, 0x3].
-	err := evm.Push(uint256.NewInt(1))
-	if err != nil {
-		t.Errorf("Push() returned an unexpected error: %v", err)
-	}
-
-	err = evm.Push(uint256.NewInt(2))
-	if err != nil {
-		t.Errorf("Push() returned an unexpected error: %v", err)
-	}
-
-	err = evm.Push(uint256.NewInt(3))
-	if err != nil {
-		t.Errorf("Push() returned an unexpected error: %v", err)
-	}
-
-	// Add the two elements at the top of the stack.
-	// The stack should be equal to [0x1, 0x5].
-	err = evm.Add()
-	if err != nil {
-		t.Errorf("Add() returned an unexpected error: %v", err)
-	}
-
-	// Pop the element from the stack to check the result.
-	result, err := evm.Pop()
-	expectedResult := uint256.NewInt(5)
-	if err != nil {
-		t.Errorf("Pop() returned an unexpected error: %v", err)
-	}
-	if !result.Eq(expectedResult) {
-		t.Errorf("Expected %v, got %v", expectedResult, result)
-	}
+	op := func(evm IEVM) error { return evm.Add() }
+	initialStack := []uint64{1, 2, 3}
+	expectedStack := []uint64{1, 5}
+	testStackOperation(t, op, nil, initialStack, expectedStack)
 }
 
-func TestAddOnEmptyStack(t *testing.T) {
-	// Create a new EVM.
-	evm := NewEVM()
-
-	// Try to add the two elements at the top of the stack.
-	err := evm.Add()
-	if err == nil {
-		t.Error("Add() should return an error because there are no elements in the stack")
-	}
+func TestMul(t *testing.T) {
+	op := func(evm IEVM) error { return evm.Mul() }
+	initialStack := []uint64{1, 2, 3}
+	expectedStack := []uint64{1, 6}
+	testStackOperation(t, op, nil, initialStack, expectedStack)
 }
 
-func TestAddOnOneElementStack(t *testing.T) {
-	// Create a new EVM.
-	evm := NewEVM()
-
-	// Add one element to the stack.
-	// The stack should be equal to [0x1].
-	err := evm.Push(uint256.NewInt(1))
-	if err != nil {
-		t.Errorf("Push() returned an unexpected error: %v", err)
-	}
-
-	// Try to add the two elements at the top of the stack.
-	err = evm.Add()
-	if err == nil {
-		t.Error("Add() should return an error because there is only one element in the stack")
-	}
+func TestStackOperationOnEmptyStack(t *testing.T) {
+	op := func(evm IEVM) error { return evm.Add() }
+	initialStack := []uint64{}
+	expectedStack := []uint64{}
+	testStackOperation(t, op, ErrStackUnderflow, initialStack, expectedStack)
 }
 
-func TestAddOnFullStack(t *testing.T) {
+func TestStackOperationOnOneElementStack(t *testing.T) {
+	op := func(evm IEVM) error { return evm.Add() }
+	initialStack := []uint64{1}
+	expectedStack := []uint64{}
+	testStackOperation(t, op, ErrStackUnderflow, initialStack, expectedStack)
+}
+
+func TestStackOperationOnFullStack(t *testing.T) {
+	op := func(evm IEVM) error { return evm.Add() }
+
+	initialStack := make([]uint64, 1024)
+	for i := range initialStack {
+		initialStack[i] = 1
+	}
+
+	expectedStack := make([]uint64, 1023)
+	copy(expectedStack, initialStack)
+	expectedStack[1022] = 2
+
+	testStackOperation(t, op, nil, initialStack, expectedStack)
+}
+
+// Helper function to test stack operations.
+func testStackOperation(t *testing.T, op func(evm IEVM) error, expectedErr error, initialStack []uint64, expectedStack []uint64) {
 	// Create a new EVM.
 	evm := NewEVM()
 
-	// Push 1024 elements to the stack.
-	// The stack should contain 1024 0x1 elements.
-	for i := 0; i < MAX_STACK_SIZE; i++ {
-		err := evm.Push(uint256.NewInt(1))
-		if err != nil {
-			t.Errorf("Push() returned an unexpected error on iteration %d: %v", i, err)
+	// Push initial elements to the stack.
+	for _, v := range initialStack {
+		if err := evm.Push(uint256.NewInt(v)); err != nil {
+			t.Errorf("Push() returned an unexpected error: %v", err)
 		}
 	}
 
-	// Try to add the two elements at the top of the stack.
-	err := evm.Add()
-	if err != nil {
-		t.Error("Add() should return an error because the stack is full")
+	// Perform the operation.
+	if err := op(evm); err != expectedErr {
+		t.Errorf("Operation returned an unexpected error: %v, wanted: %v", err, expectedErr)
+	}
+
+	// Check the stack after the operation.
+	for i := len(expectedStack) - 1; i >= 0; i-- {
+		popped, err := evm.Pop()
+		if err != nil {
+			t.Errorf("Pop() returned an unexpected error: %v", err)
+		}
+
+		expectedValue := expectedStack[i]
+		if popped == nil {
+			t.Errorf("Expected %v, got %v", expectedValue, popped)
+		} else {
+			if popped.Uint64() != expectedValue {
+				t.Errorf("Expected %v, got %v", expectedValue, popped)
+			}
+		}
 	}
 }
