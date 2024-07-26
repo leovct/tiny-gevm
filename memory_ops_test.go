c2e7495238b6994c448a8e4073aa772c0c33b204
leovct <l009.vincent@gmail.com>
2024-07-26 09:45:28 +0200

    test: improve memory tests

--- memory_ops_test.go
+++ memory_ops_test.go
@@ -32,25 +32,32 @@ func TestMLoadOnEmptyStack(t *testing.T) {
 func TestMStore(t *testing.T) {
 	op := func(evm IEVM) error { return evm.MStore() }
 
-	// Stack
+	// Initial Stack
 	// [32, 444, 2, 3]
 	offset := 32
 	initialStack := []uint64{3, 2, 444, uint64(offset)}
 
-	// Memory
+	// Initial Memory
 	// Each word is represented by 32 bytes.
 	// [111, 222, 333]
 	word1 := uint256.NewInt(111).Bytes32()
 	word2 := uint256.NewInt(222).Bytes32()
 	word3 := uint256.NewInt(333).Bytes32()
-	var memory []byte
-	memory = append(append(append(memory, word1[:]...), word2[:]...), word3[:]...)
+	var initialMemory []byte
+	initialMemory = append(append(append(initialMemory, word1[:]...), word2[:]...), word3[:]...)
 
-	// Expected
-	// - Stack: [2, 3]
-	// - Memory: [111, 444, 333]
+	// Expected Stack
+	// [2, 3]
 	expectedStack := []uint64{3, 2}
-	testStackOperationWithNewEVM(t, op, nil, initialStack, expectedStack, memory, nil, nil)
+
+	// Expected Memory
+	// Each word is represented by 32 bytes.
+	// [111, 444, 333]
+	word4 := uint256.NewInt(444).Bytes32()
+	var expectedMemory []byte
+	expectedMemory = append(append(append(expectedMemory, word1[:]...), word4[:]...), word3[:]...)
+
+	testStackOperationWithNewEVM(t, op, nil, initialStack, expectedStack, initialMemory, expectedMemory, nil)
 }
 
 func TestMStoreOnEmptyStack(t *testing.T) {
@@ -67,19 +74,26 @@ func TestMStoreOnOneElementStack(t *testing.T) {
 func TestMStore8(t *testing.T) {
 	op := func(evm IEVM) error { return evm.MStore8() }
 
-	// Stack
+	// Initial Stack
 	// [1, 444, 2, 3]
 	offset := 1
 	initialStack := []uint64{3, 2, 0x20, uint64(offset)}
 
-	// Memory
-	memory := []byte{0x01, 0x02, 0x03, 0x04}
+	// Initial Memory
+	// Each word is represented by 1 byte.
+	// [0x01, 0x02, 0x03, 0x04]
+	initialMemory := []byte{0x01, 0x02, 0x03, 0x04}
 
-	// Expected
-	// - Stack: [2, 3]
-	// - Memory: [1, 20, 3, 4]
+	// Expected Stack
+	// [2, 3]
 	expectedStack := []uint64{3, 2}
-	testStackOperationWithNewEVM(t, op, nil, initialStack, expectedStack, memory, nil, nil)
+
+	// Expected Memory
+	// Each word is represented by 1 byte.
+	// [0x01, 0x20, 0x03, 0x04]
+	expectedMemory := []byte{0x01, 0x20, 0x03, 0x04}
+
+	testStackOperationWithNewEVM(t, op, nil, initialStack, expectedStack, initialMemory, expectedMemory, nil)
 }
 
 func TestMStore8OnEmptyStack(t *testing.T) {
